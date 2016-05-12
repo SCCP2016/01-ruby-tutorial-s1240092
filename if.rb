2a22989c75e71a4898f74474ae6a5ba7ebbcda42
s1240092 <s1240092@u-aizu.ac.jp>
2016-05-12 18:55:20 +0900

    Yes No

---
+++ if.rb
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+if true then
+  puts "true is true"
+end
+
+if false then
+  puts "false is false"
+end
+
+x = 1
+
+if x >100 then
+  puts "x is 100 over"
+elsif x %2 == 0 then
+  puts "x is even"
+else
+  puts "x is odd"
+end
