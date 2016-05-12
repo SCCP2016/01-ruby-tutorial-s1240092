b9d9a3c83603a455d8c3cc3dfa8cdd5bdebb0c30
s1240092 <s1240092@u-aizu.ac.jp>
2016-05-12 19:58:13 +0900

    Yes No

---
+++ case.rb
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# case.rb
+
+
+loop{
+ a,op,b = STDIN.gets.split(' ')
+ a = a.to_i
+ b = b.to_i
+
+  case op
+  when "+"
+    puts a + b
+
+  when "-"
+    puts a - b
+  when "*"
+    puts a * b
+  when "/"
+    puts a / b
+  else
+    break
+  end
+}
