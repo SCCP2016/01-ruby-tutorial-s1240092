72f3a450e3b22561acc4bb59215b1e468cd4a61e
s1240092 <s1240092@u-aizu.ac.jp>
2016-06-30 19:42:30 +0900

    aaa

--- test.rb
+++ test.rb
@@ -1,5 +1,20 @@
-# -*- coding: undecided -*-
+# -*- coding: utf-8 -*-
 
-puts "入力してください:"
-x = gets
-puts "x=#{x}"
+people = [{name: "Mike", age: 18}, {name: "John", age: 19}, {name: "Jakky", age: 28},{name: "Mika", age: 20}, {name: "Karen", age:22}, {name: "Mary", age: 19} {name: "Chris", age: 28}, {name: "Mikky", age:25}]
+
+# 名前の頭文字がMであるハッシュのみを取り出す
+p people.select{|hash| hash[:name].chr == "M"}
+
+# 10代のハッシュのみを取り出す
+p people.select(|hash| hash[:age] < 20)
+
+# [{name: "Mike", age: 18, initial: "M"}, ...]ハッシュの配列を頭文字を加えたハッシュに変換
+
+
+# ハッシュ全員の年齢の合計を算出せよ
+p people.map{|hash| {name: hash[:name], age: hash[:age], initial: hash[:name].chr}}
+
+
+
+  each or select or inject or map
+item[:name]
